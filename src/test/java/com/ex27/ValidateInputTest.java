52dba69d2eb0ed7d1cccda02ffaa5754b047d553
Enoc Mena <enocschool@Enocs-iMac.local>
2021-07-13 13:14:20 -0400

    Initial commit

---
+++ src/test/java/com/ex27/ValidateInputTest.java
@@ -0,0 +1,14 @@
+package com.ex27;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidateInputTest {
+
+    @org.junit.jupiter.api.Test
+    void validateInput() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
